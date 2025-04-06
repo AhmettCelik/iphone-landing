110d47685bb64e1f99e4be2e5a51b393c1f5a988
“nfdiop” <ndeye-fatou.diop@berkeley.edu>
2025-04-06 18:12:26 +0200

    Code review

--- src/components/layouts/NavbarDropdownLayout.tsx
+++ src/components/layouts/NavbarDropdownLayout.tsx
@@ -5,10 +5,12 @@ type NavbarDropdownLayoutProps = {
   children: React.ReactNode;
 };
 
+// Use named export here: see why here https://dev.to/_ndeyefatoudiop/101-react-tips-tricks-for-beginners-to-experts-4m11#27-prefer-named-exports-over-default-exports
 const NavbarDropdownLayout: React.FC<NavbarDropdownLayoutProps> = ({
   isOpen,
   children,
 }) => {
+  // I would remove this state and add some delay through CSS since isVisible = isHovered
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
